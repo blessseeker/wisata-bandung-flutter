f1bc1d3ebfd4657297ad4218e8ba81071022938f
blessseeker <khoirkamaludin@gmail.com>
2021-06-06 20:15:58 +0700

    membuat responsivitas pada main screen

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -7,11 +7,87 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Wisata Bandung. Size : ${MediaQuery.of(context).size.width}'),
+          title: Text(
+              'Wisata Bandung'),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            final TourismPlace place = tourismPlaceList[index];
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return TourismPlaceList();
+          } else if (constraints.maxWidth <= 1200) {
+            return TourismPlaceGrid(gridCount: 4);
+          } else {
+            return TourismPlaceGrid(gridCount: 6);
+          }
+        }));
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailScreen(place);
+            }));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(place.imageAsset),
+                ),
+                Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(place.location)
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: tourismPlaceList.length,
+    );
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  final int gridCount;
+
+  TourismPlaceGrid({required this.gridCount});  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: GridView.count(
+          crossAxisCount: gridCount,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          children: tourismPlaceList.map((place) {
             return InkWell(
               onTap: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -19,38 +95,33 @@ class MainScreen extends StatelessWidget {
                 }));
               },
               child: Card(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
                     Expanded(
-                      flex: 1,
-                      child: Image.asset(place.imageAsset),
+                        child: Image.asset(
+                      place.imageAsset,
+                      fit: BoxFit.cover,
+                    )),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        place.name,
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                    Expanded(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Text(
-                                place.name,
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(place.location)
-                            ],
-                          ),
-                        ))
+                    Padding(
+                        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                        child: Text(place.location))
                   ],
                 ),
               ),
             );
-          },
-          itemCount: tourismPlaceList.length,
-        ));
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
